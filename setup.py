b9604b94558681a5c85ef0e559cc4edfd4ed27c0
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-12-15 02:23:16 -0800

    removed text from log

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(name='pyadtpulse',
-      version='0.2.4',
+      version='0.2.5',
       packages=[ 'pyadtpulse' ],
       description='Python interface for ADT Pulse security systems',
       long_description=long_description,
