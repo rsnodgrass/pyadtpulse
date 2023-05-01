b10f5e162a1d1b1d85005e239a8e6d01746d23f3
rlippmann <70883373+rlippmann@users.noreply.github.com>
2023-05-01 05:59:33 -0400

    Bump uvloop to 0.17

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     author="",
     author_email="",
     license="Apache Software License",
-    install_requires=["aiohttp>=3.8.1", "uvloop==0.16.0"],
+    install_requires=["aiohttp>=3.8.1", "uvloop>=0.17.0"],
     keywords=["security system", "adt", "home automation", "security alarm"],
     zip_safe=True,
     classifiers=[
