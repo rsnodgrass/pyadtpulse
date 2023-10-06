245c99b79343a98a029a98a4a09f77280fe5cd74
Robert Lippmann <rlippmann@hotmail.com>
2023-10-06 13:08:42 -0400

    add timestamp to logger in example-client

--- example-client.py
+++ example-client.py
@@ -60,7 +60,7 @@ def setup_logger(level: int):
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(level)
 
-    formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter("%(asctime)s %(name)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
